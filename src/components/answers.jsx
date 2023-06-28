e23854a24ef811aaa99fe3d6c5826cc7e48d0d7c
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-06-28 22:58:29 +0200

    add showCurrentQuestion functionality

--- src/components/answers.jsx
+++ src/components/answers.jsx
@@ -1,17 +1,43 @@
 import { useState } from "react";
 
-const Answers = ({ answers, correctAnswers }) => {
-  function handleInput(e) {
-    console.log(e.target.value);
-  }
+const Answers = ({ answers, correctAnswers, showCurrentQuestion }) => {
+  const [selectedAnswer, setSelectedAnswer] = useState("");
 
+  const [result, setResult] = useState({
+    correctAnswer: 0,
+    wrongAnswers: 0,
+  });
+
+  const onAnswerSelected = (answer) => {
+    if (answer === correctAnswers) {
+      setSelectedAnswer(true);
+      showCurrentQuestion();
+      //countMyScore();
+    } else {
+      setSelectedAnswer(false);
+      showCurrentQuestion();
+    }
+  };
+
+  /*
+  const countMyScore = () => {
+    setResult((prev) =>
+      selectedAnswer
+        ? {
+            ...prev,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
+    );
+  };
+*/
   return (
     <div className="flex justify-center">
-      {answers.map((answer) => (
+      {answers.map((answer, index) => (
         <button
-          onClick={(e) => handleInput(e, "value")}
+          onClick={(e) => onAnswerSelected(e, answer)}
           value={answer}
-          key={answer.id}
+          key={index}
           className="m-2 rounded-md bg-orange-400 p-4 font-sans font-bold text-white shadow-inner hover:bg-amber-500 hover:text-gray-900"
         >
           {answer}
---
+++ src/components/quizResults.jsx
@@ -0,0 +1,10 @@
+const QuizResults = () => {
+  return (
+    <div>
+      <h1>Your Result</h1>
+      <p>3 of 6 correct answers</p>
+    </div>
+  );
+};
+
+export default QuizResults;
