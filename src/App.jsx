eb816dcde31ba276e313b3d0d3fefdbdcf2bc6ce
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-06-28 10:31:58 +0200

    add api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import Answers from "./components/answers";
+import axios from "axios";
 import Questions from "./components/questions";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://wd40-trivia.onrender.com/api/questions")
+      .then((response) => {
+        setPost(response.data);
+      });
+  }, []);
+
   return (
     <>
-      <div className="flex flex-col items-center ">
+      <div className="flex flex-col items-center mb-10">
         <h1 className="text-8xl text-white mb-10">Quiz App</h1>
-        <Questions />
+        <Questions questions={post} />
       </div>
     </>
   );
