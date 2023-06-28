a80dfd3c849c82491820401fd6aea847341710cd
Omid Amini <oamini1994@gmail.com>
2023-06-28 12:41:00 +0200

    fix-error

--- src/components/answers.jsx
+++ src/components/answers.jsx
@@ -7,11 +7,11 @@ const Answers = ({ answers, correctAnswers }) => {
 
   return (
     <div className="flex justify-center">
-      {answers.map((answer) => (
+      {answers.map((answer, index) => (
         <button
-          onClick={(e) => handleInput(e, "value")}
+          onClick={(e) => handleInput(e, answer)}
           value={answer}
-          key={answer.id}
+          key={index}
           className="m-2 cursor-pointer rounded-md bg-orange-400 p-4 font-sans font-bold text-white shadow-inner hover:bg-amber-500 hover:text-gray-900"
         >
           {answer}
