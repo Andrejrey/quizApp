fd0f947dfa4ddeb7562f685eaa7ebfec810eac95
Omid Amini <oamini1994@gmail.com>
2023-06-29 13:24:15 +0200

    refactoring-the hard codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
-import Questions from "./components/questions";
 import { useEffect, useState } from "react";
+import Questions from "./components/Questions";
 
 function App() {
-  const [post, setPost] = useState([]);
+  const [questions, setQuestions] = useState([]);
   const [result, setResult] = useState({
     correctAnswerResult: 0,
     wrongAnswersResult: 0,
@@ -13,17 +13,15 @@ function App() {
     axios
       .get("https://wd40-trivia.onrender.com/api/questions")
       .then((response) => {
-        setPost(response.data);
+        setQuestions(response.data);
       });
   }, []);
 
   return (
-    <>
-      <div className="flex flex-col items-center mb-10">
-        <h1 className="text-8xl text-white mb-10">Quiz App</h1>
-        <Questions questions={post} setResult={setResult} result={result} />
-      </div>
-    </>
+    <div className="mb-10 flex flex-col items-center">
+      <h1 className="mb-10 text-8xl text-white">Quiz App</h1>
+      <Questions questions={questions} setResult={setResult} result={result} />
+    </div>
   );
 }
 
--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -1,51 +1,39 @@
 import { useState } from "react";
-import Answers from "./answers";
-import QuizResults from "./quizResults";
+import Answers from "./Answers";
+import QuizResults from "./QuizResults";
 
 const Questions = ({ questions, setResult, result }) => {
   const [showResult, setShowResult] = useState(false);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
-  function showCurrentQuestion() {
-    if (currentQuestionIndex > 4) {
+  const showCurrentQuestion = () => {
+    if (currentQuestionIndex >= questions.length - 1) {
       setShowResult(true);
     } else {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
     }
-  }
+  };
 
   return (
     <div>
       {!showResult ? (
-        <>
-          {questions.map((question, index) => {
-            return (
-              <div key={question.id}>
-                {index === currentQuestionIndex ? (
-                  <>
-                    <div>
-                      <p className="m-5 flex justify-center font-sans text-xl text-white">
-                        {question.question}
-                      </p>
-                      <Answers
-                        answers={question.answers}
-                        correctAnswers={question.correctAnswer}
-                        uresAnswer={question.userAnswer}
-                        answersId={question.id}
-                        questionsIndex={index}
-                        showCurrentQuestion={showCurrentQuestion}
-                        key={question.answers}
-                        setResult={setResult}
-                      />
-                    </div>
-                  </>
-                ) : (
-                  ""
-                )}
+        questions.map((question, index) => (
+          <div key={question.id}>
+            {index === currentQuestionIndex && (
+              <div>
+                <p className="m-5 flex justify-center font-sans text-xl text-white">
+                  {question.question}
+                </p>
+                <Answers
+                  answers={question.answers}
+                  correctAnswer={question.correctAnswer}
+                  showCurrentQuestion={showCurrentQuestion}
+                  setResult={setResult}
+                />
               </div>
-            );
-          })}
-        </>
+            )}
+          </div>
+        ))
       ) : (
         <QuizResults result={result} />
       )}
