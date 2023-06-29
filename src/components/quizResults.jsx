d4037d0980787f6cb6185da09a452e97205e18e3
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-06-29 12:51:40 +0200

    add restart button

--- src/components/quizResults.jsx
+++ src/components/quizResults.jsx
@@ -1,4 +1,8 @@
 const QuizResults = ({ result }) => {
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
   return (
     <div className="flex flex-col justify-center items-center text-white text-xl">
       <h1>Your Result</h1>
@@ -9,6 +13,12 @@ const QuizResults = ({ result }) => {
           ? result.wrongAnswersResult + " your idiot!!!"
           : result.wrongAnswersResult}
       </p>
+      <button
+        className="m-2 mt-8 rounded-md w-72 bg-orange-400 p-4 font-sans font-bold text-white shadow-inner hover:bg-amber-500 hover:text-gray-900"
+        onClick={refreshPage}
+      >
+        Start a new quiz!
+      </button>
     </div>
   );
 };
