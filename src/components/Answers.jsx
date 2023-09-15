134763425efcfa4a7f2813a3bed57f71b6e4c757
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-09-15 22:20:31 +0200

    remove one unnecessary map and useState and add show correct and wrong answers function

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -3,34 +3,58 @@ const Answers = ({
   correctAnswer,
   showCurrentQuestion,
   setResult,
+  currentQuestion,
+  setQuestionsWithCorrectAnswer,
+  questionsWithCorrectAnswer,
+  setQuestionsWithWrongAnswer,
+  questionsWithWrongAnswer,
 }) => {
   const onAnswerSelected = (answer) => {
     if (answer === correctAnswer) {
       setResult((prev) => ({
         ...prev,
-        correctAnswerResult: prev.correctAnswerResult + 1,
+        correctAnswersResult: prev.correctAnswersResult + 1,
       }));
+      setQuestionsWithCorrectAnswer([
+        ...questionsWithCorrectAnswer,
+        {
+          question: currentQuestion,
+          answer: answer,
+        },
+      ]);
     } else {
       setResult((prev) => ({
         ...prev,
         wrongAnswersResult: prev.wrongAnswersResult + 1,
       }));
+      setQuestionsWithWrongAnswer([
+        ...questionsWithWrongAnswer,
+        {
+          question: currentQuestion,
+          userAnswer: answer,
+          correctAnswer: correctAnswer,
+        },
+      ]);
     }
     showCurrentQuestion();
   };
 
+  console.log("correct:", questionsWithCorrectAnswer);
+  console.log("wrong", questionsWithWrongAnswer);
+
   return (
     <div className="flex flex-col items-center justify-center">
-      {answers.map((answer, index) => (
-        <button
-          onClick={() => onAnswerSelected(answer, index)}
-          value={answer}
-          key={index}
-          className="m-2 w-72 rounded-md bg-orange-400 p-4 font-sans font-bold text-white shadow-inner hover:bg-amber-500 hover:text-gray-900"
-        >
-          {answer}
-        </button>
-      ))}
+      {answers &&
+        answers.map((answer, index) => (
+          <button
+            onClick={() => onAnswerSelected(answer)}
+            value={answer}
+            key={index}
+            className="m-2 w-72 rounded-md bg-orange-400 p-4 font-sans font-bold text-white shadow-inner hover:bg-amber-500 hover:text-gray-900"
+          >
+            {answer}
+          </button>
+        ))}
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="mb-6 flex flex-col items-center">
+    <div className="mt-6 mb-6 flex flex-col items-center">
       <h1 className="mb-10 text-8xl text-white">Quiz App</h1>
     </div>
   );
